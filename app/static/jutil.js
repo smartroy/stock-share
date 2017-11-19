498345f679403ff9664444fb1651e5671a0c1fdd
smartroy <xushikang1127@gmail.com>
2017-11-18 23:23:27 -0500

    view product at new order

--- app/static/jutil.js
+++ app/static/jutil.js
@@ -13,6 +13,7 @@ $(function() {
                                 '<tr>'+
                                 '<th>Brand</th>'+
                                 '<th>Name/Nick name</th>'+
+                                '<th>Color</th>'+
                                 '<th>New Qty.</th>'+
                                 '<th>Price</th>'+
                                 '<th>Note</th>'+
@@ -23,10 +24,12 @@ $(function() {
                 $("div#sources_result").append("<h2>UPC not found</h2>");
             }else{
                 for (i=0;i<data.length;i++){
-                    table_text += ("<tr id='"+data[i]["_id"]+"''>"+
+                    var link_addr = '{{url_for("main.product_details",product_id ='+data[i]["_id"]+')}}>';
+                    table_text += ("<tr name='"+data[i]["_id"]+"''>"+
 
                         "<td name='brand'>"+data[i]["brand"]+"</td>"+
-                        "<td name='nick_name'>"+data[i]["name"]+"/"+data[i]["nick_name"]+"</td>"+
+                        "<td name='nick_name'><a href=\"/product/product_details/"+data[i]["_id"]+"\" target=\"_blank\">"+data[i]["name"]+"/"+data[i]["nick_name"]+"</a></td>"+
+                        "<td name='color'>"+data[i]["color"]+"</td>"+
                         "<td><input type='text' name='qty' size=4 value=0></td>"+
                         "<td><input type='text' name='price' size=4 value=0.0></td>"+
                         "<td><input type='text' name='note' size=4 style='width:100%;'></td>"+
@@ -46,15 +49,17 @@ $(function() {
                         var item_qty=$(this).find('[name="qty"]').val();
                         if(parseInt(item_qty)>0){
                             // add_table +=$(this).html();
-                            var item_id =$(this).attr('id');
+                            var item_id =$(this).attr('name');
                             var item_brand = $(this).find('[name="brand"]').text();
                             var item_name=$(this).find('[name="nick_name"]').text();
+                            var item_color = $(this).find('[name="color"]').text();
                             var item_price=$(this).find('[name="price"]').val();
                             var item_note=$(this).find('[name="note"]').val();
                             add_table += ("<tr id='"+item_id+"''>"+
 
                                     "<td name='brand'>"+item_brand+"</td>"+
                                     "<td name='nick_name'>"+item_name+"</td>"+
+                                    "<td name='color'>"+item_color+"</td>"+
                                     "<td><input type='text' name='qty' size=4 value="+item_qty+"></td>"+
                                     "<td><input type='text' name='price' size=4 value="+item_price+"></td>"+
                                     "<td><input type='text' name='note' size=4 style='width:100%;' value="+item_note+"></td>"+
